cd5b873dd630382b270951007941b72ecf22db2b
rafaeldamasceno <rafaeldamasceno@me.com>
2018-11-28 16:48:55 +0000

    Unprocessed orders page is being show!
    Pretty!

--- matesc/src/components/UnprocessedOrders.js
+++ matesc/src/components/UnprocessedOrders.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 import {
-    Container
+    Container,
+    Row,
+    Col,
+    Input,
+    Table,
+    Button
 } from 'reactstrap';
 
 class UnprocessedOrders extends Component {
     render() {
-        return <Container>yay</Container>
+        return (<Container>
+            <Row>
+                <Col>
+                    <h1>Orders</h1>
+                </Col>
+                <Col xs='0' className='ml-auto'>
+                    <Input type='text' placeholder='Search'></Input>
+                </Col>
+            </Row>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Client</th>
+                        <th>Deadline</th>
+                        <th className='text-center'>Exclude</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">36272</th>
+                        <td>Bertrand NorteShopping</td>
+                        <td>21/02/2019</td>
+                        <td className='text-center pl-5'><Input type='checkbox'></Input></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">62378</th>
+                        <td>FNAC Braga</td>
+                        <td>12/03/2019</td>
+                        <td className='text-center pl-5'><Input type='checkbox'></Input></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">62153</th>
+                        <td>Papelaria Mundo</td>
+                        <td>30/01/2019</td>
+                        <td className='text-center pl-5'><Input type='checkbox'></Input></td>
+                    </tr>
+                </tbody>
+            </Table>
+            <Button outline color='primary' size='lg' className='float-right'>Create picking wave</Button>
+        </Container>)
     }
 }
 
