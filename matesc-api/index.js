c35ed899ca98ccb766bc8e72710d919117997644
rafaeldamasceno <rafaeldamasceno@me.com>
2018-11-29 18:02:49 +0000

    Add dijkstra and nodemon.

--- matesc-api/index.js
+++ matesc-api/index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const graph = require('node-dijkstra')
+const warehouse = new graph()
+
+app.get('/picking-wave', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+warehouse.addNode('PC', {
+    '11': 1,
+    '21': 1,
+    '31': 1,
+    '41': 1,
+    '51': 1,
+})
+
+warehouse.addNode('11', {
+    'PC': 1,
+    '12': 1,
+    '21': 1,
+    '22': 1
+})
+
+console.log(warehouse.path('PC', '12'));
