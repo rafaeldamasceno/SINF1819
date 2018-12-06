5bcf6e99ee88d07df19e36c40f3f2c487e1c52c8
up201508537 <up201508537@fe.up.pt>
2018-12-06 18:25:05 +0000

    Implemented LowDB, tested routes and database.

--- matesc-api/index.js
+++ matesc-api/index.js
@@ -5,8 +5,46 @@ const port = 3001
 const graph = require('node-dijkstra')
 const warehouse = new graph()
 
-app.get('/picking-wave', (req, res) => res.send('Hello World!'))
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
+db.defaults({
+	pickingWaves:[],
+	resupplyWaves:[]
+}).write()
+
+app.get('/picking-wave', (req, res) => 
+	{
+		let results = {}
+		results.waveId = db.get('pickingWaves').size().value() + 1
+		db.get('pickingWaves').push({id:results.waveId}).write()
+		res.send(results)
+	})
+	
+app.get('/resupply-wave', (req, res) => 
+	{
+		let results = {}
+		results.waveId = db.get('resupplyWaves').size().value() + 1
+		db.get('resupplyWaves').push({id:results.waveId}).write()
+		res.send(results)
+	})
+	
+app.get('/picking-wave/:id', (req, res) => 
+	{
+		let results = {}
+		results = db.get('pickingWaves').find({id:parseInt(req.params.id)}).value()
+		res.send(results)
+	})
+	
+app.get('/resupply-wave/:id', (req, res) => 
+	{
+		let results = {}
+		results = db.get('resupplyWaves').find({id:parseInt(req.params.id)}).value()
+		res.send(results)
+	})
+	
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 warehouse.addNode('PC', {
