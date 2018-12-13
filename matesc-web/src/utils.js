67c67b59adc1fb4197535e61920ce67fde014da2
mmoedinhas <mmoedinhas@hotmail.com>
2018-12-13 17:18:44 +0000

    fetching all items from checked orders

--- matesc-web/src/utils.js
+++ matesc-web/src/utils.js
@@ -3,7 +3,8 @@ import {
     Alert,
   } from 'reactstrap';
 
-export const API_URL = 'http://localhost:2018/WebApi';
+export const PRIMAVERA_URL = 'http://localhost:2018/WebApi';
+export const MATESC_URL = 'http://localhost:3001';
 
 export function makeRequestBody(body) {
     let formData = new URLSearchParams();
@@ -16,7 +17,7 @@ export function makeRequestBody(body) {
 };
 
 export function authenticate() {
-    return fetch(`${API_URL}/token`, {
+    return fetch(`${PRIMAVERA_URL}/token`, {
         method: 'POST',
         body: makeRequestBody(
             {
@@ -31,13 +32,6 @@ export function authenticate() {
     });
 }
 
-/*export function makeHeaders(authentication) {
-    return {
-        'Authorization' : `Bearer ${authentication['access_token']}`,
-        'Accept': 'application/json'
-    };
-}*/
-
 export function makeHeaders(authentication) {
     return {
         'Authorization' : `Bearer ${authentication['access_token']}`,
@@ -46,7 +40,7 @@ export function makeHeaders(authentication) {
 }
 
 export function unprocessedClientOrdersFetch(authentication){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify("SELECT CONCAT(CD.Serie, CD.NumDoc) as OrderId, CD.Entidade, CD.Nome, CD.Data, CDS.Estado FROM CabecDoc CD INNER JOIN CabecDocStatus CDS ON CDS.IdCabecDoc = CD.Id AND CD.TipoDoc = 'ECL' AND CDS.Anulado = 'false' AND CDS.Fechado = 'false' AND CDS.Estado = 'P'")
@@ -54,7 +48,7 @@ export function unprocessedClientOrdersFetch(authentication){
 }
 
 export function unprocessedSuppliersOrdersFetch(authentication){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify("SELECT CONCAT(CC.Serie, CC.NumDoc) as OrderId, CC.Entidade, CC.Nome, CC.DataDoc, CCS.Estado FROM CabecCompras CC INNER JOIN CabecComprasStatus CCS ON CCS.IdCabecCompras = CC.Id AND CC.TipoDoc = 'ECF' AND CCS.Anulado = 'false' AND CCS.Fechado = 'false' AND CCS.Estado = 'P'"
@@ -63,7 +57,7 @@ export function unprocessedSuppliersOrdersFetch(authentication){
 }
 
 export async function supplierOrderInfoContent(authentication, Doc_Serie, Doc_Number){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify(`SELECT CONCAT(CC.Serie, CC.NumDoc) as OrderId, CC.DataDoc, CC.Nome, CC.Entidade, SUM(LC.PrecUnit*LC.Quantidade + LC.TotalIva) as PrecoTotal FROM LinhasCompras LC INNER JOIN CabecCompras CC ON LC.IdCabecCompras = CC.Id WHERE CC.Serie = '${Doc_Serie}' AND CC.NumDoc = ${Doc_Number} AND CC.TipoDoc = 'ECF' GROUP BY CC.DataDoc, CC.Nome, CC.Entidade, CC.Id, CC.Serie, CC.NumDoc`
@@ -72,7 +66,7 @@ export async function supplierOrderInfoContent(authentication, Doc_Serie, Doc_Nu
 }
 
 export function supplierOrderContent(authentication, Doc_Serie, Doc_Number){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify(`SELECT ArmazemLocalizacoes.Descricao as DescricaoLocalizacao, Artigos.* FROM ArmazemLocalizacoes INNER JOIN (SELECT ArtigosLinhas.*, (Artigo.Peso * ArtigosLinhas.Quantidade) as PesoTotal, (Artigo.Volume * ArtigosLinhas.Quantidade) as VolumeTotal, Artigo.Peso as PesoUnit, Artigo.Volume as VolumeUnit FROM Artigo INNER JOIN (SELECT LC.Artigo, LC.Descricao, LC.Localizacao, LC.Quantidade FROM LinhasCompras LC INNER JOIN CabecCompras CC ON LC.IdCabecCompras = CC.Id WHERE CC.Serie = '${Doc_Serie}' AND CC.NumDoc = ${Doc_Number} AND CC.TipoDoc = 'ECF') as ArtigosLinhas ON ArtigosLinhas.Artigo = Artigo.Artigo) as Artigos ON ArmazemLocalizacoes.Localizacao = Artigos.Localizacao`
@@ -81,7 +75,7 @@ export function supplierOrderContent(authentication, Doc_Serie, Doc_Number){
 }
 
 export async function clientOrderInfoContent(authentication, Doc_Serie, Doc_Number){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify(`SELECT CONCAT(CD.Serie, CD.NumDoc) as OrderId, CD.Data, CD.Nome, CD.Entidade, SUM(LD.PrecUnit*LD.Quantidade + LD.TotalIva) as PrecoTotal FROM LinhasDoc LD INNER JOIN CabecDoc CD ON LD.IdCabecDoc = CD.Id WHERE CD.Serie = '${Doc_Serie}' AND CD.NumDoc = ${Doc_Number} AND CD.TipoDoc = 'ECL' GROUP BY CD.Data, CD.Nome, CD.Entidade, CD.Id, CD.Serie, CD.NumDoc`
@@ -90,7 +84,7 @@ export async function clientOrderInfoContent(authentication, Doc_Serie, Doc_Numb
 }
 
 export function clientOrderContent(authentication, Doc_Serie, Doc_Number){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify(`SELECT ArmazemLocalizacoes.Descricao as DescricaoLocalizacao, Artigos.* FROM ArmazemLocalizacoes INNER JOIN (SELECT ArtigosLinhas.*, (Artigo.Peso * ArtigosLinhas.Quantidade) as PesoTotal, (Artigo.Volume * ArtigosLinhas.Quantidade) as VolumeTotal, Artigo.Peso as PesoUnit, Artigo.Volume as VolumeUnit FROM Artigo INNER JOIN (SELECT LD.Artigo, LD.Descricao, LD.Localizacao, LD.Quantidade FROM LinhasDoc LD INNER JOIN CabecDoc CD ON LD.IdCabecDoc = CD.Id WHERE CD.Serie = '${Doc_Serie}' AND CD.NumDoc = ${Doc_Number} AND CD.TipoDoc = 'ECL') as ArtigosLinhas ON ArtigosLinhas.Artigo = Artigo.Artigo) as Artigos ON ArmazemLocalizacoes.Localizacao = Artigos.Localizacao`
@@ -99,7 +93,7 @@ export function clientOrderContent(authentication, Doc_Serie, Doc_Number){
 }
 
 export function itemsInStock(authentication){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify(`SELECT ArmazemLocalizacoes.Descricao as DescricaoLocalizacao, ArtigoArmazem.* FROM ArmazemLocalizacoes INNER JOIN (SELECT Inv.Artigo, A.Descricao as Nome, Inv.Localizacao as Localizacao, ISNULL(Inv.StkActual, 0) AS StkActual FROM V_INV_ArtigoArmazem Inv INNER JOIN Artigo A ON Inv.Artigo = A.Artigo WHERE Inv.StkActual IS NOT NULL AND Inv.StkActual > 0) as ArtigoArmazem ON ArmazemLocalizacoes.Localizacao = ArtigoArmazem.Localizacao`
@@ -108,12 +102,19 @@ export function itemsInStock(authentication){
 }
 
 export function itemsOutOfStock(authentication){
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method: 'POST',
         headers: makeHeaders(authentication),
         body:JSON.stringify(`SELECT ArmazemLocalizacoes.Descricao as DescricaoLocalizacao, ArtigoArmazem.* FROM ArmazemLocalizacoes INNER JOIN (SELECT Inv.Artigo, A.Descricao as Nome, Inv.Localizacao as Localizacao, ISNULL(Inv.StkActual, 0) AS StkActual FROM V_INV_ArtigoArmazem Inv INNER JOIN Artigo A ON Inv.Artigo = A.Artigo WHERE Inv.StkActual IS NULL OR Inv.StkActual = 0) as ArtigoArmazem ON ArmazemLocalizacoes.Localizacao = ArtigoArmazem.Localizacao`
         )
     });
+}
+
+export function createPickingWave(orders) {
+    return fetch(`${MATESC_URL}/picking-wave`, {
+        method: 'POST',
+        body:JSON.stringify(orders)
+    })
 }
 
 
@@ -128,14 +129,14 @@ export function errorMessage(error){
 
 
 export function loadItems(authentication) {
-    return fetch(`${API_URL}/Base/Artigos/LstArtigos`, {
+    return fetch(`${PRIMAVERA_URL}/Base/Artigos/LstArtigos`, {
         headers: makeHeaders(authentication)
     });
 }
 
 
 export function query(authentication, query) {
-    return fetch(`${API_URL}/Administrador/Consulta`, {
+    return fetch(`${PRIMAVERA_URL}/Administrador/Consulta`, {
         method:'POST',
         headers: makeHeaders(authentication),
         body: `${query}`
