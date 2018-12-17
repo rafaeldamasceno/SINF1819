ac75b606c9d68b8665df65bfe283ce84a859b59c
mmoedinhas <mmoedinhas@hotmail.com>
2018-12-17 21:03:57 +0000

    removed unused dependency

--- matesc-web/src/components/UnprocessedOrders.js
+++ matesc-web/src/components/UnprocessedOrders.js
@@ -3,7 +3,6 @@ import {
     Container,
     Button
 } from 'reactstrap';
-import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import SearchableTableCheckbox from "./SearchableTableCheckbox";
 import NavBar from "../NavBar"
