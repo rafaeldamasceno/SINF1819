9f235fa15357df6d44ed6c1f9f806ed2ec60d3c7
catFerreira <up201506671@fe.up.pt>
2018-12-19 13:54:32 +0000

    login is cuter

--- matesc-web/src/Login.js
+++ matesc-web/src/Login.js
@@ -7,10 +7,10 @@ import {
     Alert,
 } from 'reactstrap';
 import Cookies from 'universal-cookie';
-import { authenticate} from './utils';
+import { authenticate } from './utils';
 
 export default class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             loading: false,
@@ -20,72 +20,68 @@ export default class Login extends Component {
         this.validateInput = this.validateInput.bind(this);
     }
 
-    showLoadingOrButton(){
-        if(this.state.loading)
+    showLoadingOrButton() {
+        if (this.state.loading)
             return <div className="loader">Loading...</div>
         else
-            return <Button>Submit</Button>
+            return <button className="btn btn-lg btn-primary btn-block" name="Submit" value="Login" type="Submit">Login</button>
     }
 
-    loginSuccess(){
+    loginSuccess() {
         window.location.href = '/unprocessed-client-orders';
     }
 
-   async validateInput(event){
+    async validateInput(event) {
         event.preventDefault();
         this.setState({
-            loading:true
+            loading: true
         })
         let response = await authenticate(this.username.value, this.password.value);
         this.setState({
-            loading:false
+            loading: false
         })
-        if(response.status === 200){
+        if (response.status === 200) {
             let token = await response.json();
             const cookies = new Cookies();
-            cookies.set('token', token, { path: '/' , maxAge: token.expires_in});
+            cookies.set('token', token, { path: '/', maxAge: token.expires_in });
             this.loginSuccess();
-        }else{
+        } else {
             this.setState({
-                error:true
-            }) 
-        }       
-        
+                error: true
+            })
+        }
+
     }
 
-    showWrongCredencials(){
-        if(this.state.error)
-            return <Alert color="danger"> Username/Password Errada</Alert>
+    showWrongCredencials() {
+        if (this.state.error)
+            return <Alert color="danger">Wrong Username/Password</Alert>
     }
 
     render() {
         return (<Container className="Login">
-            {this.showWrongCredencials()}
-            <h2>Log In</h2>
-            <Form className="form" onSubmit={this.validateInput}>
-                <Col>
-                    <FormGroup>
-                        <input className="form-control" 
-                            ref={(username) => this.username = username} 
-                            type="text"
-                            id="username"
-                            placeholder="Username"
-                        />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                    <input className="form-control" 
-                            ref={(password) => this.password = password} 
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                        />
-                    </FormGroup>
-                </Col>
-                {this.showLoadingOrButton()}
-            </Form>
+            <div className="wrapper">
+                <Form className="form-signin" onSubmit={this.validateInput}>
+                    <h3 className="form-signin-heading">Welcome Back! Please Sign In</h3>
+                    <hr className="colorgraph"></hr>
+
+                    <input className="form-control"
+                        ref={(username) => this.username = username}
+                        type="text"
+                        id="username"
+                        placeholder="Username"
+                    />
+                    <input className="form-control"
+                        ref={(password) => this.password = password}
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                    />
+    	            {this.showWrongCredencials()}
+                    {this.showLoadingOrButton()}
+                </Form>
+            </div>
         </Container>
-        );
+        )
     }
 }
